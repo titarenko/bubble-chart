3c09aa5027a96cafd2a57a5ac1ceda4e97126221
Constantin Titarenko <constantin.titarenko@gmail.com>
2013-03-22 21:53:30 +0700

    Added link to demo.

---
+++ build.js
@@ -0,0 +1,9 @@
+({
+	baseUrl: ".",
+	paths: {
+		d3: "http://cdnjs.cloudflare.com/ajax/libs/d3/3.0.8/d3.min",
+		jquery: "http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min"
+	},
+	name: "client",
+	out: "client-built.js"
+})
